b17aa6450b3d5f39b8214d0096e07ec86eff2938
Mikmondi <mikmondi@gmail.com>
2021-06-15 15:13:16 +0300

    linked the user component to the services

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
+import { GithubService } from '../github-service/github.service';
 
 @Component({
   selector: 'app-user',
@@ -7,10 +8,21 @@ import { User } from '../user';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+  user!: User;
+   constructor(private userService:GithubService ) { }
+   usersearch(textsearch: string){
+     this.userService.userRequest(textsearch).then(
+       ()=>{
+         this.user=this.userService.user;
+ 
+       },
+       (error)=>{
+         console.log(error)
+       }
+     )
+   }
+   ngOnInit(): void {
+     this.userService.userRequest("brendahuwitonze")
+     this.user = this.userService.user
+   }
+ }
